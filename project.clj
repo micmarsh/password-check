b6bd668e05e4dcfcd671499caab547e4b46cd7a5
micmarsh <mike@marsh.pw>
2014-09-03 20:30:05 -0400

    more formatting and remove stateful thing

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   org.clojars.liquidz/clj-password-check "0.0.1"
   :description "password checker library for clojure"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  )
+                 [org.clojure/clojure-contrib "1.2.0"]])
--- test/clj_password_check/test/core.clj
+++ test/clj_password_check/test/core.clj
@@ -7,20 +7,14 @@
     (are [x y] (= x y)
       true  (f "str")
       true  (f 12)
-      false (f '(1 2))
-      )
-    )
-  )
+      false (f '(1 2)))))
 
 (deftest test-combine-checkers-and
   (let [f (combine-checkers-and string? (fn [s] (= \a (first s))))]
     (are [x y] (= x y)
       true  (f "abc")
       false (f "bac")
-      false (f 123)
-      )
-    )
-  )
+      false (f 123))))
 
 (deftest test-contains-uppercase?
   (are [x y] (= x y)
@@ -28,9 +22,7 @@
     false (contains-uppercase? "a")
     false (contains-uppercase? "0")
     false (contains-uppercase? "!")
-    false (contains-uppercase? "")
-    )
-  )
+    false (contains-uppercase? "")))
 
 (deftest test-contains-lowercase?
   (are [x y] (= x y)
@@ -38,9 +30,7 @@
     true  (contains-lowercase? "a")
     false (contains-lowercase? "0")
     false (contains-lowercase? "!")
-    false (contains-lowercase? "")
-    )
-  )
+    false (contains-lowercase? "")))
 
 (deftest test-contains-alphabet?
   (are [x y] (= x y)
@@ -48,9 +38,7 @@
     true  (contains-alphabet? "a")
     false (contains-alphabet? "0")
     false (contains-alphabet? "!")
-    false (contains-alphabet? "")
-    )
-  )
+    false (contains-alphabet? "")))
 
 (deftest test-contains-number?
   (are [x y] (= x y)
@@ -58,14 +46,11 @@
     false (contains-number? "a")
     true  (contains-number? "0")
     false (contains-number? "!")
-    false (contains-number? "")
-    )
-  )
+    false (contains-number? "")))
 
 (deftest test-length-range
   (let [f (length-range 3 4)
-        g (length-range 3)
-        ]
+        g (length-range 3)]
     (are [x y] (= x y)
       false (f "")
       false (f "aa")
@@ -76,16 +61,12 @@
       false (g "aa")
       true  (g "aaa")
       true  (g "aaaa")
-      true  (g (apply str (take 10000 (repeat "a"))))
-      )
-    )
-  )
+      true  (g (apply str (take 10000 (repeat "a")))))))
 
 (deftest test-not-same-characters?
   (is (not-same-characters? "aaaaAaaa"))
   (is (not (not-same-characters? "aaaaaaaa")))
-  (is (not (not-same-characters? "")))
-  )
+  (is (not (not-same-characters? ""))))
 
 (deftest test-not-sequential-password?
   (are [x y] (= x y)
@@ -96,9 +77,7 @@
     false (not-sequential-password? "0123456")
     false (not-sequential-password? "6543210")
     true  (not-sequential-password? "0123457")
-    true  (not-sequential-password? "")
-    )
-  )
+    true  (not-sequential-password? "")))
 
 (deftest test-contains-symbol?
   (are [x y] (= x y)
@@ -106,9 +85,7 @@
     false (contains-symbol? "a")
     false (contains-symbol? "0")
     true  (contains-symbol? "!")
-    false (contains-symbol? "")
-    )
-  )
+    false (contains-symbol? "")))
 
 (deftest test-not-contains-multi-byte-character?
   (are [x y] (= x y)
@@ -116,26 +93,4 @@
     true  (not-contains-multi-byte-character? "!@#")
     false (not-contains-multi-byte-character? "はろー")
     true  (not-contains-multi-byte-character? "123")
-    true  (not-contains-multi-byte-character? "")
-    )
-  )
-
-(deftest test-last-checker
-  (contains-alphabet? "a")
-  (is (= 'contains-lowercase? @last-checker))
-  (contains-alphabet? "A")
-  (is (= 'contains-uppercase? @last-checker))
-
-  (let [f (combine-checkers not-blank? (length-range 2 4) not-same-characters? not-sequential-password?)]
-    (f "")
-    (is (= 'not-blank? @last-checker))
-    (f "a")
-    (is (= 'length-range @last-checker))
-    (f "aaa")
-    (is (= 'not-same-characters? @last-checker))
-    (f "abcd")
-    (is (= 'not-sequential-password? @last-checker))
-    (f "0123456")
-    (is (= 'length-range @last-checker))
-    )
-  )
+    true  (not-contains-multi-byte-character? "")))
